45e280af9169bf91eb040d56c18830e1781dac1a
ksio2124 <ksio2124@protonmail.ch>
2024-07-08 13:37:19 -0700

    Add client form validation on guests

--- little_lemon/src/BookingForm.js
+++ little_lemon/src/BookingForm.js
@@ -1,33 +1,37 @@
 import { useEffect, useState } from "react";
-import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
 
 function BookingForm({availableTimes, updateTimes, submitForm}) {
-    const [date, setDate] = useState(availableTimes.date);
     const [time, setTime] = useState(availableTimes.times[0]);
     const [guests, setGuests] = useState(1);
     const [occasion, setOccasion] = useState('birthday');
+    const [guestError, setGuestError] = useState('');
     const handleSubmit = (e) => {
         submitForm(e);
         e.preventDefault();
     }
 
     useEffect(() => {
-        setDate(availableTimes.date);
         setTime(availableTimes.times[0])
     }, [availableTimes])
+
+    const handleOnGuestOnChange = (e) => {
+        const num = e.target.value;
+        setGuestError( num > 10 || num < 1 ? 'Need to be between 1 and 10 inclusively' : '');
+        setGuests(num);
+    }
     return (
         <div style={{width: '100%', display: 'flex', justifyContent:'center', padding: '4%'}}>
-        <form style={{display: 'grid', maxWidth: '200px', gap: '20px'}} onSubmit={handleSubmit}>
+        <form style={{display: 'grid', width: '200px', gap: '10px'}} onSubmit={handleSubmit}>
             <label htmlFor="res-date">Choose date</label>
-            {/* <DatePicker id="res-date" onChange={(date) => updateTimes(date)} selected={date} /> */}
             <input type="date"
                 id="res-date"
-                value={date.toISOString().split('T')[0]}
+                value={availableTimes.date.toISOString().split('T')[0]}
+                style={{width: '100%'}}
                 onChange={(e) => {
                     updateTimes((new Date(e.target.value)))}
                 } />
-            <label htmlFor="res-time">Choose time</label>
+            <label htmlFor="res-time" >Choose time</label>
             <select id="res-time" value={time} onChange={(e) => setTime(e.target.value)}>
                 {availableTimes.times.map((time) => {
                     return (
@@ -36,7 +40,8 @@ function BookingForm({availableTimes, updateTimes, submitForm}) {
                 })}
             </select>
             <label htmlFor="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={(e) => setGuests(e.target.value)}/>
+            <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={handleOnGuestOnChange}/>
+            {guestError !== '' && <p style={{color: 'red', width: '100%'}}>{guestError}</p> }
             <label htmlFor="occasion">Occasion</label>
             <select value={occasion} id="occasion" onChange={(e) => {setOccasion(e.target.value)}}>
                 <option value="birthday" >Birthday</option>
